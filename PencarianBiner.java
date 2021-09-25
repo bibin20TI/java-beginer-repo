c373748e350666d8f81d8fa287e66cce0027e33a
bibin20TI <bibin.aripin_ti20@nusaputra.ac.id>
2021-09-26 04:15:42 +0700

    menambahkan kondisi jika data yang di cari lebih kecil dari pada data tengah

--- PencarianBiner.java
+++ PencarianBiner.java
@@ -6,11 +6,22 @@ class PencarianBiner {
           int data[] = {2,5,6,9,12,15,20};
 
           int tengah = data.length/2;
-          int cari = 9;
+          int cari = 5;
           boolean ketemu = false;
            
-           if(data[tengah] == cari){
+           if(cari == data[tengah]){
            	 System.out.print("data di temukan pada indek ke - "+tengah);
+           	 ketemu = true;
+           }else {
+           	   if(cari < data[tengah]){
+           	   	for(int i = tengah-1; i >= 0; i--){
+           	   		if(cari == data[i]){
+           	   			System.out.print("data di temukan pada indek ke - "+i);
+           	   			ketemu = true;
+           	   			break;
+           	   		}
+           	   	}
+           	   }
            }
           if(ketemu == false){
 
