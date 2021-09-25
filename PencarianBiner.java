7822b1ce0bcb91bd8a37044d6d6f13517dd330aa
bibin20TI <bibin.aripin_ti20@nusaputra.ac.id>
2021-09-26 04:23:25 +0700

    menambahkan komentar agar lebih jelas

--- PencarianBiner.java
+++ PencarianBiner.java
@@ -9,12 +9,12 @@ class PencarianBiner {
           int cari = 100;
           boolean ketemu = false;
            
-           if(cari == data[tengah]){
+           if(cari == data[tengah]){                                         // jika data yang di cari sama dengan tengah
            	 System.out.print("data di temukan pada indek ke - "+tengah);
            	 ketemu = true;
            }else {
            	   if(cari < data[tengah]){
-           	   	for(int i = tengah-1; i >= 0; i--){
+           	   	for(int i = tengah-1; i >= 0; i--){              // jika data yang di cari lebih kecil dari data tengah
            	   		if(cari == data[i]){
            	   			System.out.print("data di temukan pada indek ke - "+i);
            	   			ketemu = true;
@@ -25,14 +25,14 @@ class PencarianBiner {
            	   	   for(int j = tengah+1; j < data.length; j++){
            	   	   	 if(cari == data[j]){
            	   	   	 	System.out.print("data di temukan pada indek ke - "+j);
-           	   	   	 	ketemu = true;
+           	   	   	 	ketemu = true;                // jika data yang di cari lebih besar dari data tengah
            	   	   	 	break;
            	   	   	 }
            	   	   }
            	   }
-           }
+           } 
           if(ketemu == false){
-
+                                               // jika data yang di cari tidak ada dalam array
           	System.out.print("data tidak di temukan !!!");
           }
           
